249f7ff90dba3830eac8c6dfbfd316fe7e1a49c6
fyzhu <i@zhuyunfeng.com>
2020-03-11 21:39:39 +0800

    fix

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-import './style/base.sass'
-import './vendor'
+import "./style/base.sass";
+import "./vendor";
 
 // app.js
 App({
-  async getUserInfo () {
-    if (this.globalData.userInfo) return this.globalData.userInfo
+  async getUserInfo() {
+    if (this.globalData.userInfo) return this.globalData.userInfo;
 
-    const { code } = await wx.loginAsync()
-    const { userInfo } = await wx.getUserInfoAsync()
+    const { code } = await wx.loginAsync();
+    const { userInfo } = await wx.getUserInfoAsync();
 
-    this.globalData.userInfo = userInfo
+    this.globalData.userInfo = userInfo;
   },
 
   globalData: {
     userInfo: null,
-    qiniuCDN: 'http://osmai097y.bkt.clouddn.com',
-    serverUrl: 'http://127.0.0.1:3000'
+    qiniuCDN: "http://ice-fire-img.dreamlist.cn",
+    serverUrl: "http://127.0.0.1:3002"
   }
-})
+});
